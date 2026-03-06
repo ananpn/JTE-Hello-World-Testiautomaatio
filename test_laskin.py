cc1615278b878c6f1df8c5500630e8e334bb9e98
ananpn <150531931+ananpn@users.noreply.github.com>
2026-03-06 16:49:34 +0200

    Break test_laskin.py
    Test breaking the python test file

--- test_laskin.py
+++ test_laskin.py
@@ -3,7 +3,7 @@ from laskin import plus
 
 class TestLaskin(unittest.TestCase):
     def test_plus(self):
-        self.assertEqual(plus(1, 1), 2)
+        self.assertEqual(plus(1, 1), 3)
 
 if __name__ == '__main__':
     unittest.main()
